abd331ca3b125baeb79e6caffb14f94a2c391bb1
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-17 02:17:20 +0100

    a

--- src/jokesData.js
+++ src/jokesData.js
@@ -8,3 +8,9 @@ const jokesData = [
         question: "What's the best thing about Switzerland?",
         punchLine: "I don't know, but the flag is a big plus!"
     },
+    {
+        id: 3,
+        question: "Did you hear about the mathematician who's afraid of negative numbers?",
+        punchLine: "He'll stop at nothing to avoid them!"
+    },
+    
