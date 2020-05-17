9775057e3031e065f0e397b6721557127702dfb1
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-17 02:04:36 +0100

    a

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import JokeList from './components/JokeList';
+import Joke from './components/Joke';
 
 function App() {
   return (
     <div className="App">
 
       <Header title="Random Jokes"/>
+      <Joke />
       <JokeList />  
     </div>
   );
