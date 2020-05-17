e87384a400fdcceb4034e34a20a68c06d6877748
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-17 02:17:05 +0100

    a

--- src/jokesData.js
+++ src/jokesData.js
@@ -3,3 +3,8 @@ const jokesData = [
         id: 1,
         punchLine: "It’s hard to explain puns to kleptomaniacs because they always take things literally."
     },
+    {
+        id: 2,
+        question: "What's the best thing about Switzerland?",
+        punchLine: "I don't know, but the flag is a big plus!"
+    },
