e034f8b712cf2fcdbf17073c72f3ff817b3be788
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-20 01:58:36 +0100

    a

--- src/App.js
+++ src/App.js
@@ -10,31 +10,7 @@ function App() {
       } )
   return (
     <div className="App">
-
       <Header title="Random Jokes Ooops"/>
-    
-        <Joke punchline="i say your" />
-        <Joke
-          question="You look Ugly"
-          punchline="I was trying to look like today"
-        />
-        <Joke
-          question="I slept with ur mom yesterday night"
-          punchline="Now we're even!"
-        />
-        <Joke
-          question="Ask me out"
-          punchline="Get Out !!!"
-        />
-        <Joke
-          question="You like the way i speak ??"
-          punchline="Dude, you speak utter crap !!!"
-        />
-        <Joke
-          question="You failed !!!"
-          punchline="Same way ur fathers condom failed... "
-        />
-   
     </div>
   );
 }
