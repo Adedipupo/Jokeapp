f5bce568b83137991cf64fe5b18367b1f88b21f7
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-20 02:06:38 +0100

    a

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Joke from './components/Joke';
 import jokesData from './jokesData';
 
 function App() {
-      jokesData.map(joke => {
-        return (<Joke question={joke.question} punchline={joke.punchLine}/>)
-      } )
+      jokesData.map(joke => <Joke question={joke.question} punchline={joke.punchLine}/>)
   return (
     <div className="App">
       <Header title="Random Jokes Ooops"/>
