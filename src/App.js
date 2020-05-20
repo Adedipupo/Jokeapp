be4c6bae8fd2fe21ddde9b1cf98f970e211d8d80
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-20 01:53:52 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import JokeList from './components/JokeList';
+import Joke from './components/Joke'
 
 function App() {
   return (
