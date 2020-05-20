7d62126d732246ef9016486cf15eb814246a42ed
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-20 01:53:17 +0100

    a

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -4,32 +4,12 @@ import jokesData from '../jokesData';
 
 const JokeList = () => {
     jokesData.map({
-        
+        return(
+            <Joke />
+        )
     })
 return(
-  <div>
-        <Joke punchline="i say your" />
-        <Joke
-            question="You look Ugly"
-            punchline="I was trying to look like today"
-        />
-        <Joke
-            question="I slept with ur mom yesterday night"
-            punchline="Now we're even!"
-        />
-        <Joke
-            question="Ask me out"
-            punchline="Get Out !!!"
-        />
-        <Joke
-            question="You like the way i speak ??"
-            punchline="Dude, you speak utter crap !!!"
-        />
-        <Joke
-            question="You failed !!!"
-            punchline="Same way ur fathers condom failed... "
-        />
-  </div>
+ 
 )
 }
 
